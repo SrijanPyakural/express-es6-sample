a844f5e9cc97889fd510a2fde0655a77fa2b0b77
srijan <srijanpyakurel@gmail.com>
2025-10-31 15:27:01 +0000

    initial

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World' });
+  res.render('index', { title: 'My Student ID is 22049933. Welcome to Express' });
 });
+
 
 export default router;
